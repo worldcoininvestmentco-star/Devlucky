6acd683927b1ee5488a2720030de6edaa2f8e174
worldcoininvestmentco-star <worldcoininvestment.co@gmail.com>
2025-08-29 18:02:10 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "lucky~IiUiyAZa#od4r3S9CK2iQ7a0NYm-EOz2jSxSdMMZUtuqKWMWOOa8",  // add your Session Id here
+  SESSION_ID: "lucky~QyElDYzb#3Ss8C02Qka6CDzFRXu8oYZy7hIfOoKWuTlOPOG4v4uc",  // add your Session Id here
   OWNER_NUMBER: "256789966218", // put your phone number here
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   TIMEZONE: "Africa/Kampala" //put your country timeZone....leave blank if u don't know.
